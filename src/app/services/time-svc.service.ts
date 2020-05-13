6756c63ad40d94b5048b399005412de312628319
irreprehensible <irreprehensible@gmail.com>
2020-05-13 17:21:14 +0530

    server test

--- src/app/services/time-svc.service.ts
+++ src/app/services/time-svc.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
+import { DataSVCService } from './data-svc.service';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TimeSVCService {
+export class TimeSVCService extends DataSVCService{
+  constructor(http:HttpClient){
+    super('http://worldtimeapi.org/api/timezone/Europe/London',http)
+  }
   getGMT(){
     console.log('[timnesvc] hit server');
+    let obj
     let today =new Date();
-    return new Date(new Date(today.getFullYear(),today.getMonth(),today.getDate(),today.getHours()-4,today.getMinutes()-30,today.getSeconds()));
+    
+  this.getAll().subscribe(response => {
+    obj = response;
+    console.log(Date.parse(obj.datetime))
+  })
+  return new Date(new Date(today.getFullYear(),today.getMonth(),today.getDate(),today.getHours()-4,today.getMinutes()-30,today.getSeconds()));
   }
 }

a005754d084ca6d6aeb17f80a0cb61aaeb9e64a3
irreprehensible <irreprehensible@gmail.com>
2020-05-14 12:28:02 +0530

    time svc update to get from api included promise

--- src/app/components/time/time.component.ts
+++ src/app/components/time/time.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TimeSVCService } from 'src/app/services/time-svc.service';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'time',
@@ -7,25 +8,28 @@ import { TimeSVCService } from 'src/app/services/time-svc.service';
   styleUrls: ['./time.component.css']
 })
 export class TimeComponent implements OnInit {
-  time:string
+  time:string="--:--:--"
   timeSVC:TimeSVCService;
   constructor( private _time:TimeSVCService) {
     this.timeSVC = _time;
-  
-   }
-   checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
   }
+   
   ngOnInit(): void {
-     this.setTime(this.timeSVC.getGMT());
+    //  this.setTime(this.timeSVC.getGMT(),function() {
+    //    console.log('[]');
+    //  });
+    // this.timeSVC.getGMT(function(today){
+    //   console.log('[callback]',today)
+    //   this.setTime(today)
+    // })
+    this.timeSVC.getGMT().then(today => this.setTime(today)).catch(err => throwError(err))
   }
-  setTime(today) {
+  public setTime(today) {
     let timeNow = new Date(today.getFullYear(),today.getMonth(),today.getDate(),today.getHours(),today.getMinutes(),today.getSeconds())
     let int = setInterval(() => {
-      if((timeNow.getMinutes() - today.getMinutes()) >=5){
+      if((timeNow.getMinutes() - today.getMinutes()) >=1){
         clearInterval(int);
-        this.setTime(this.timeSVC.getGMT());
+        this.timeSVC.getGMT().then(today => this.setTime(today));
       }
       else{
         timeNow.setSeconds(timeNow.getSeconds()+1); 
@@ -35,5 +39,9 @@ export class TimeComponent implements OnInit {
         this.time = h + ":" + m + ":" + s;   
       }
     }, 1000);
+  }
+  private checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
   }
 }
--- src/app/services/time-svc.service.ts
+++ src/app/services/time-svc.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { DataSVCService } from './data-svc.service';
 import { HttpClient, HttpResponse } from '@angular/common/http';
+import { error } from '@angular/compiler/src/util';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +13,24 @@ export class TimeSVCService extends DataSVCService{
   getGMT(){
     console.log('[timnesvc] hit server');
     let obj
-    let today =new Date();
-    
-  this.getAll().subscribe(response => {
-    obj = response;
-    console.log(Date.parse(obj.datetime))
-  })
-  return new Date(new Date(today.getFullYear(),today.getMonth(),today.getDate(),today.getHours()-4,today.getMinutes()-30,today.getSeconds()));
+    const p = new Promise((resolve,reject)=>{
+      this.getAll().subscribe(response => {
+        obj = response;
+        // console.log(obj.datetime.split('T')[1].split('.')[0])
+        let y = obj.datetime.split('T')[0].split('-')[0];
+        let m = Number(obj.datetime.split('T')[0].split('-')[1])-1
+        let d =obj.datetime.split('T')[0].split('-')[2]
+        let hh = obj.datetime.split('T')[1].split('.')[0].split(':')[0]
+        let mm = obj.datetime.split('T')[1].split('.')[0].split(':')[1]
+        let ss = obj.datetime.split('T')[1].split('.')[0].split(':')[2]
+        let da = new Date(y,m,d,hh,mm,ss)
+        console.log(da)
+        resolve(da)
+      },err=>{
+        reject(new Error(err))
+      })
+    });
+    return p;
+  //return today; //new Date(new Date(today.getFullYear(),today.getMonth(),today.getDate(),today.getHours()-4,today.getMinutes()-30,today.getSeconds()));
   }
 }

20533fe0098b7a8119b51be154f4463b706fe0c4
irreprehensible <irreprehensible@gmail.com>
2020-05-22 17:51:01 +0530

    sub mnu trial erroe

--- src/app/components/button-link/button-link.component.ts
+++ src/app/components/button-link/button-link.component.ts
@@ -12,17 +12,32 @@ export class ButtonLinkComponent implements OnInit {
   @Input() title:string
   @Input() type:string
   showBtn:boolean
+  subMnu:boolean
+  subMnuId:any =''
   listItems
   @HostListener('window:click', ['$event.target'])
   onClick(targetElement:HTMLElement) {
-    if(!targetElement.classList.contains('button-link') )
+    // console.log(targetElement)
+    
+    if(!targetElement.classList.contains('button-link') && !targetElement.classList.contains('button-link-sub') ){
       this.showBtn=false
+      this.subMnuId='';
+    }
   }
   constructor(private _trainSVC:TrainsSVCService) { }
-  toggleUserNav(){
+  toggleUserNav(event){
+    //console.log(event.target.classList.value.indexOf('button-link-sub'))
+    if(event.target.classList.value.indexOf('button-link-sub')<0)
     this.showBtn = !this.showBtn;
   }
-
+  showSubmenu(event,item){
+    this.subMnuId=item.id;
+    console.log(event);
+  }
+  subMenuAction(){
+    this.subMnuId='';
+    console.log('submenu action')
+  }
   ngOnInit(): void {
     this._trainSVC.getTrainList().then(trains => this.listItems=trains)
   }
